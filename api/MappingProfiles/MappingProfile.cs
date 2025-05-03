56ff9640fa8e902ec6420f931f2c082adf0274a1
AS-PATRICE <simopatrice@yahoo.fr>
2025-05-03 10:12:55 +0200

    Mise en placa du fichier de mappage

---
+++ api/MappingProfiles/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using api.DTOs;
+using api.Models;
+using AutoMapper;
+
+namespace api.MappingProfiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserDto, ApplicationUser>().ReverseMap();
+            CreateMap<BookDto, Book>().ReverseMap();
+            CreateMap<LoanDto, Loan>().ReverseMap();
+            CreateMap<NotificationDto, Notification>().ReverseMap();
+        }
+    }
+}
