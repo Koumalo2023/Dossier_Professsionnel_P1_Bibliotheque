5395f40948959ea4626e75179c33bd0479462cf2
Patrice SIMO <simopatrice@yahoo.fr>
2025-05-03 20:49:09 +0200

    Ajoute la table au DbContextmodèle pour stocker les refresh tokens

---
+++ api/Models/ApplicationUser/RefreshToken.cs
@@ -0,0 +1,17 @@
+﻿namespace api.Models
+{
+    public class RefreshToken
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; } 
+        public string Token { get; set; }
+        public DateTime Expires { get; set; }
+        public DateTime Created { get; set; }
+        public string CreatedByIp { get; set; }
+        public bool IsExpired => DateTime.UtcNow >= Expires;
+        public bool IsActive => !IsExpired;
+
+        // Relation avec l'utilisateur
+        public ApplicationUser User { get; set; }
+    }
+}
