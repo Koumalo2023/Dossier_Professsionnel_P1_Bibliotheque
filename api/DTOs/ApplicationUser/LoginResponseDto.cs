112c343cb51801f2f6ee1f35f6fa36a0c4954440
Patrice SIMO <simopatrice@yahoo.fr>
2025-05-03 20:48:49 +0200

    Modification des Dtos

---
+++ api/DTOs/ApplicationUser/LoginResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace api.DTOs
+{
+    public class LoginResponseDto
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime TokenExpires { get; set; }
+        public DateTime RefreshTokenExpires { get; set; }
+    }
+}
---
+++ api/DTOs/ApplicationUser/RefreshTokenRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.DTOs
+{
+    public class RefreshTokenRequestDto
+    {
+        [Required]
+        public string Token { get; set; }
+        [Required]
+        public string RefreshToken { get; set; }
+    }
+}
