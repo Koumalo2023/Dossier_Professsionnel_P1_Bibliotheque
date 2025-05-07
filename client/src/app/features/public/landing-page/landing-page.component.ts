216edd028b562d5b067db317252ed003186be4ab
Patrice SIMO <simopatrice@yahoo.fr>
2025-05-07 10:19:29 +0200

    Mise en place de la Loanding Page

--- client/src/app/features/public/landing-page/landing-page.component.ts
+++ client/src/app/features/public/landing-page/landing-page.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-    selector: 'app-landing-page',
-    imports: [],
-    templateUrl: './landing-page.component.html',
-    styleUrl: './landing-page.component.scss'
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
 
