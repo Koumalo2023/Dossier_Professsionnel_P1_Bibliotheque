0932ef2ff0517cc93d7d36f10b3e17902ea4432d
Patrice SIMO <simopatrice@yahoo.fr>
2025-10-09 10:31:22 +0200

    Remplacement d'Angular Material par Bootstrap et Font Awesome

---
+++ client/src/app/features/user/reading-goals-page/reading-goals-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reading-goals-page',
+  imports: [],
+  templateUrl: './reading-goals-page.component.html',
+  styleUrl: './reading-goals-page.component.scss'
+})
+export class ReadingGoalsPageComponent {
+
+}
---
+++ client/src/app/features/user/reading-history-page/reading-history-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reading-history-page',
+  imports: [],
+  templateUrl: './reading-history-page.component.html',
+  styleUrl: './reading-history-page.component.scss'
+})
+export class ReadingHistoryPageComponent {
+
+}
